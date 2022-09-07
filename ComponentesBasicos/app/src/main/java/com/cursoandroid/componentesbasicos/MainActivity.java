6ea9b6bc472cc688ff11c2ddb360b2d87c210e57
MarcioSousa <marciofrapeso@gmail.com>
2022-09-07 13:38:01 -0300

    Caixa de selecao.

--- ComponentesBasicos/app/src/main/java/com/cursoandroid/componentesbasicos/MainActivity.java
+++ ComponentesBasicos/app/src/main/java/com/cursoandroid/componentesbasicos/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -12,21 +13,66 @@ import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText campoNome;
+    private TextInputEditText campoEmail;
+    private TextView textoResultado;
+
+    private CheckBox cbxVerde, cbxBranco, cbxVermelho;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        campoNome = findViewById(R.id.editNome);
+        campoEmail = findViewById(R.id.editEmail);
+        textoResultado = findViewById(R.id.textResultado);
+
+        //Checkbox
+        cbxVerde = findViewById(R.id.cbxVerde);
+        cbxBranco = findViewById(R.id.cbxBranco);
+        cbxVermelho = findViewById(R.id.cbxVermelho);
+    }
+
+    public void checkbox(){
+        String texto = "";
+
+
+        if(cbxVerde.isChecked()){
+            texto = "Verde selecionado - ";
+            //String corSelecionada = cbxVerde.getText().toString();
+            //texto = corSelecionada;
+        }
+
+        if(cbxBranco.isChecked()){
+            texto = texto + "Branco selecionado - ";
+        }
+
+        if(cbxVermelho.isChecked()){
+            texto = texto + "Vermelho selecionado - ";
+        }
+
+
+
+        textoResultado.setText(texto);
+
     }
 
     public void enviar(View view){
-        EditText campoNome = findViewById(R.id.editNome);
-        TextInputEditText campoEmail = findViewById(R.id.editEmail);
-        TextView textoResultdo = findViewById(R.id.textResultado);
-
+        /*
         String nome = campoNome.getText().toString();
         String email = campoEmail.getText().toString();
-        
-        textoResultdo.setText("Nome: " + nome + ", e-mail: " + email);
+
+        textoResultado.setText("Nome: " + nome + ", e-mail: " + email);
+        */
+
+        checkbox();
+
+    }
+
+    public void limpar(View view){
+        campoNome.setText("");
+        campoEmail.setText("");
     }
 
 }
