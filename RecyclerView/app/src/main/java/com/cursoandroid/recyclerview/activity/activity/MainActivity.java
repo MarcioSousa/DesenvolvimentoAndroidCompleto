0eb816c5e41aa72944f7bb6924b547c510523228
MarcioSousa <marciofrapeso@gmail.com>
2022-09-13 22:14:20 -0300

    ReciclerView - Parte 1 e Parte 2.

---
+++ RecyclerView/app/src/main/java/com/cursoandroid/recyclerview/activity/activity/MainActivity.java
@@ -0,0 +1,34 @@
+package com.cursoandroid.recyclerview.activity.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.cursoandroid.recyclerview.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        //Configurar adapter
+
+
+        //Configurar RecyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        //recyclerView.setAdapter();
+
+    }
+
+
+}
