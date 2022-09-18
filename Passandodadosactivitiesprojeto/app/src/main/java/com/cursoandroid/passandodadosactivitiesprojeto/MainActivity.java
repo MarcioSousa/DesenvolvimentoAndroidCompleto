a8a5c1ba0ba8a9a8f2652259201d01c97adcd4da
MarcioSousa <marciofrapeso@gmail.com>
2022-09-17 22:28:47 -0300

    Passando dados entre activities

---
+++ Passandodadosactivitiesprojeto/app/src/main/java/com/cursoandroid/passandodadosactivitiesprojeto/MainActivity.java
@@ -0,0 +1,39 @@
+package com.cursoandroid.passandodadosactivitiesprojeto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonEnviar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonEnviar = findViewById(R.id.buttonEnviar);
+
+        buttonEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SegundaActivity.class);
+
+                //Passar dados
+                intent.putExtra("nome","Marcio Sousa");
+                intent.putExtra("idade",30);
+
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+
+
+}
