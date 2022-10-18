0763950c530d5b546118ddde4dfd68f353e55250
MarcioSousa <marciofrapeso@gmail.com>
2022-10-18 16:56:01 -0300

    139 - Finalizando App9 Minhas Anotações.

--- MinhasAnotacoes/app/src/main/java/com/cursoandroid/minhasanotacoes/MainActivity.java
+++ MinhasAnotacoes/app/src/main/java/com/cursoandroid/minhasanotacoes/MainActivity.java
@@ -2,6 +2,7 @@ package com.cursoandroid.minhasanotacoes;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,26 +18,57 @@ import com.cursoandroid.minhasanotacoes.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
+    private AnotacaoPreferencias preferencias;
+    private EditText editAnotacao;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        editAnotacao = findViewById(R.id.editAnotacao);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        preferencias = new AnotacaoPreferencias(getApplicationContext());
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+
+        //binding = ActivityMainBinding.inflate(getLayoutInflater());
+        //setContentView(binding.getRoot());
+
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Validar se o usuário digitor algo.
+                String textoRecuperado = editAnotacao.getText().toString();
+
+                if(textoRecuperado.equals("")){
+                    Snackbar.make(view, "Preencha a anotação", Snackbar.LENGTH_LONG).show();
+                }else{
+                    preferencias.salvarAnotacao(textoRecuperado);
+                    Snackbar.make(view, "Anotação salva com sucesso!", Snackbar.LENGTH_LONG).show();
+                }
+
+                //Snackbar.make(view, "Preencha a anotação", Snackbar.LENGTH_LONG)
+                //        .setAction("Action", null).show();
+
             }
         });
+
+        //Recuperar a anotação
+        String anotacao = preferencias.recuperarAnotacao();
+
+        //Verificar se tem alguma anotação gravada.
+        if(!anotacao.equals("")){
+            editAnotacao.setText(anotacao);
+        }
+
     }
 
 }
