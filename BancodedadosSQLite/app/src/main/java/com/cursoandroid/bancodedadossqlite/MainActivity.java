07015af1b0568166645f880dc0c92381b069e108
MarcioSousa <marciofrapeso@gmail.com>
2022-10-19 21:37:02 -0300

    146 - Atualizando e removendo registros.

--- BancodedadosSQLite/app/src/main/java/com/cursoandroid/bancodedadossqlite/MainActivity.java
+++ BancodedadosSQLite/app/src/main/java/com/cursoandroid/bancodedadossqlite/MainActivity.java
@@ -20,11 +20,23 @@ public class MainActivity extends AppCompatActivity {
             SQLiteDatabase bancoDados = openOrCreateDatabase("app", MODE_PRIVATE, null);
 
             //CRIAR TABELA
-            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas (nome VARCHAR, idade INT(3))");
+            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas (" +
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "nome VARCHAR, " +
+                    "idade INT(3))");
+            //bancoDados.execSQL("DROP TABLE pessoas");
 
             //INSERIR DADOS
-            //bancoDados.execSQL("INSERT INTO pessoas (nome, idade) VALUES('Marcio Sousa', 22)");
-            //bancoDados.execSQL("INSERT INTO pessoas (nome, idade) VALUES('Gabriel Paz', 32)");
+            //bancoDados.execSQL("INSERT INTO pessoas (nome, idade) VALUES('Marcio', 22)");
+            //bancoDados.execSQL("INSERT INTO pessoas (nome, idade) VALUES('Gabriel', 32)");
+            //bancoDados.execSQL("INSERT INTO pessoas (nome, idade) VALUES('Rafael', 24)");
+            //bancoDados.execSQL("INSERT INTO pessoas (nome, idade) VALUES('Maria', 41)");
+            //bancoDados.execSQL("INSERT INTO pessoas (nome, idade) VALUES('João', 45)");
+
+            //ATUALIZAR E DELETAR DADOS
+            //bancoDados.execSQL("UPDATE pessoas SET idade = 10 WHERE id = 1");
+            //bancoDados.execSQL("DELETE FROM pessoas WHERE id = 5");
+            bancoDados.execSQL("DELETE FROM pessoas");
 
             //RECUPERAR PESSOAS
             /*String consulta =
@@ -50,23 +62,28 @@ public class MainActivity extends AppCompatActivity {
                             "WHERE nome LIKE 'mar%'";
             String consulta =
                     "SELECT nome, idade FROM pessoas " +
-                            "WHERE 1=1 ORDER BY idade ASC";*///DESC
+                            "WHERE 1=1 ORDER BY idade ASC";//DESC
             String consulta =
                     "SELECT nome, idade FROM pessoas " +
-                            "WHERE 1=1 ORDER BY idade DESC LIMIT 2";
+                            "WHERE 1=1 ORDER BY idade DESC LIMIT 2";*/
+            String consulta =
+                    "SELECT * FROM pessoas";
+                    //"SELECT id, nome, idade FROM pessoas";
+
             Cursor cursor = bancoDados.rawQuery(consulta, null);
 
             //INDICES DA TABELA
+            int indiceId = cursor.getColumnIndex("id");
             int indiceNome = cursor.getColumnIndex("nome");
             int indiceIdade = cursor.getColumnIndex("idade");
 
             cursor.moveToFirst();
             while (cursor != null){
-
+                String id = cursor.getString(indiceId);
                 String nome = cursor.getString(indiceNome);
                 String idade = cursor.getString(indiceIdade);
 
-                Log.i("RESULTADO - nome ", nome + " , idade: " + idade);
+                Log.i("RESULTADO - id ", id + "; nome: " + nome + " ; idade: " + idade);
                 cursor.moveToNext();
             }
 
