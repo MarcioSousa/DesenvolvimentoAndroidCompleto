1e32e4022a993fc7a1b1782a9f2449724b1300fc
MarcioSousa <marciofrapeso@gmail.com>
2022-10-31 22:15:05 -0300

    167 - Recuperando dados.

--- FirebaseApp/app/src/main/java/com/cursoandroid/firebaseapp/MainActivity.java
+++ FirebaseApp/app/src/main/java/com/cursoandroid/firebaseapp/MainActivity.java
@@ -1,12 +1,17 @@
 package com.cursoandroid.firebaseapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     //private DatabaseReference referencia = FirebaseDatabase.getInstance().getReference("usuarios");
@@ -17,13 +22,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //DatabaseReference usuarios = referencia.child("usuarios");
+        //Recuperando somente um dado.
+        //DatabaseReference usuarios = referencia.child("usuarios").child("001");
+        DatabaseReference usuarios = referencia.child("usuarios");
         DatabaseReference produtos = referencia.child("produtos");
 
-        //Usuario usuario = new Usuario();
-        //usuario.setNome("Maria");
-        //usuario.setSobrenome("Souza");
-        //usuario.setIdade(22);
+        //Recuperar dados do Firebase
+        usuarios.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //É chamado sempre que conseguir recuperar os dados e alterados.
+                Log.i("Firebase", snapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                //Cancelamento de requisição
+
+            }
+        });
+
+        /*Usuario usuario = new Usuario();
+        usuario.setNome("Maria");
+        usuario.setSobrenome("Souza");
+        usuario.setIdade(22);
 
         Produto produto1 = new Produto();
         Produto produto2 = new Produto();
@@ -43,13 +65,16 @@ public class MainActivity extends AppCompatActivity {
 
         produtos.child("001").setValue(produto1);
         produtos.child("002").setValue(produto2);
-        produtos.child("003").setValue(produto3);
+        produtos.child("003").setValue(produto3);*/
 
         //usuarios.child("002").setValue(usuario);
 
         //referencia.child("pontos").setValue("100");
         //referencia.child("usuarios2").child("002").child("nome").setValue("Marcio Sousa");
         //referencia.child("usuarios2").child("002").child("nome").setValue("Marcio Sousa");
+
+
+
     }
 
 }
